6ea7966057f831afb978ad51a861f569575eab14
yigittopm <yigittopm@hotmail.com>
2021-05-10 21:35:40 +0300

    regex email test

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -2,6 +2,16 @@ const pool = require("../helpers/database");
 
 const register = async (req,res,next) => {
     const { username, password, email} = await req.body;
+
+    //test
+    const email_regex = /([a-zA-Z0-9_]+)@([a-zA-Z]+)(\.[a-z]{2,})/g;
+
+    if(email_regex.test(email)){
+        res.json("Başarılı")
+    }else{
+        res.json("Hatalı")
+    }
+
     const data = await pool.query("Insert into users (username, password, email) values ($1, $2, $3)", [username, password, email]);
     res.json(data);
 }
