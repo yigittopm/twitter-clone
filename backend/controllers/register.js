9768f23f62b2e53970a46216b551c1a755f90053
yigittopm <yigittopm@hotmail.com>
2021-05-12 12:31:56 +0300

    regex ok

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -4,14 +4,26 @@ const register = async (req,res,next) => {
     const { username, password, email} = await req.body;
 
     //test
+    const username_regex = /^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$/g;
     const email_regex = /([a-zA-Z0-9_]+)@([a-zA-Z]+)(\.[a-z]{2,})/g;
+    const password_regex = /^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$/g;
 
-    if(email_regex.test(email)){
-        const data = await pool.query(process.env.DB_INSERT, [username, password, email]);
-        res.json(data)
-    }else{
-        res.json("Hatalı")
+    if(!username_regex.test(username)){
+        res.status(401).json("Username yanlış formda.");
     }
+
+    if(!email_regex.test(email)){
+        res.status(401).json("Email yanlış formda.");
+    }
+
+    if(!password_regex.test(password)){
+        res.status(401).json("Password yanlış formda.");
+    }
+
+    const data = await pool.query(process.env.DB_INSERT, [username, password, email]);
+
+    res.json({success: "true", data})
+
 }
 
 module.exports = {
