6025511469182c00371d7e679e818239ae00fe62
yigittopm <yigittopm@hotmail.com>
2021-05-13 18:31:32 +0300

    added errorResponse

--- backend/utils/ErrorResponse.js
+++ backend/utils/ErrorResponse.js
@@ -1,6 +1,6 @@
 class ErrorResponse extends Error{
-    constructor(messages, statusCode){
-        super(messages);
+    constructor(message, statusCode){
+        super(message);
         this.statusCode = statusCode;
     }
 }
